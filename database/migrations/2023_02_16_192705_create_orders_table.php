e45f54f87d7c941ed8222887dd985a33bf10d757
Amir <rasti2001@yahoo.com>
2023-02-16 23:20:45 +0330

    all-tables-created

--- database/migrations/2023_02_16_192705_create_orders_table.php
+++ database/migrations/2023_02_16_192705_create_orders_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->char('mobile',11);
-            $table->enum('role',['user','admin']);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('amount');
+            $table->char('ref_code');
+            $table->enum('status',['unpaid, paid']);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
