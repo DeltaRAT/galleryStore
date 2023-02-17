babe4ba4cf94f2fb4b96e48002052bc23bc2af83
Amir <rasti2001@yahoo.com>
2023-02-17 19:18:39 +0330

    Categories-almost-finished

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Categories\CreateRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -11,15 +12,22 @@ class CategoriesController extends Controller
     public function create(){
         return view('admin.categories.create');
     }
-    public function store(Request $request){
+    public function store(CreateRequest $request){
+        $validatedData = $request->validated() ;
         $createdCategory = Category::create([
-           'title' => $request->title,
-            'slug' => $request->slug
+           'title' => $validatedData['title'],
+            'slug' => $validatedData['slug']
         ]);
         if (!$createdCategory){
             return back()->with('failed','عملیات موفقیت آمیز نبود:(');
         }
         return back()->with('success', 'عملیات موفقیت آمیز بود:)');
+    }
+
+    public function all(){
+        $categories = Category::all();
+
+        return view('admin.categories.all', compact('categories'));
     }
 
 }
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -10,7 +10,7 @@
                     <h1 class="m-0 text-dark">
                         <a class="nav-link drawer" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
                         دسته بندی ها / افزودن
-                        <a class="btn btn-primary float-left text-white py-2 px-4" href="category.php">بازگشت به صفحه دسته بندی ها</a>
+                        <a class="btn btn-primary float-left text-white py-2 px-4" href="{{ route('admin.categories.all') }}">بازگشت به صفحه دسته بندی ها</a>
                     </h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -21,11 +21,7 @@
     <!-- Main content -->
     <div class="content">
         <div class="container-fluid">
-            @if(session('failed'))
-                <div class="alert alert-danger">{{ session('failed') }}</div>
-            @elseif(session('success'))
-                <div class="alert alert-success">{{ session('success') }}</div>
-            @endif
+            @include('errors.message')
             <div class="row mt-5">
                 <div class="col-md-12">
                     <div class="card card-defualt">
