2a970e1f4dccc4dff952bbe6928adcb56fc7862b
Amir <rasti2001@yahoo.com>
2023-02-23 15:16:54 +0330

    delete-method-implemented

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::prefix('admin')->group(function(){
        Route::post('',[ProductsController::class,'store'])->name('admin.products.store');
         Route::get('{product_id}/download/demo',[ProductsController::class,'downloadDemo'])->name('admin.products.download.demo');
         Route::get('{product_id}/download/source',[ProductsController::class,'downloadSource'])->name('admin.products.download.source');
-
+        Route::delete('{product_id}/delete',[ProductsController::class, 'delete'])->name('admin.products.delete');
    });
 });
 
