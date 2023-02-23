58c501ef804ec205f5cd1fc1c12c9da0d016e27c
Amir <rasti2001@yahoo.com>
2023-02-23 13:54:11 +0330

    all.blade-implemented

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -14,16 +14,25 @@ use App\utilities\ImageUploader;
 
 class ProductsController extends Controller
 {
-    public function create(){
-        $categories = Category::all();
+    public function all()
+    {
+        $products = Product::paginate(10);
 
-        return view('admin.products.all',compact('categories'));
+        return view('admin.products.all', compact('products'));
     }
 
-    public function store(CreateRequest $request){
+    public function create()
+    {
+        $categories = Category::all();
+
+        return view('admin.products.add', compact('categories'));
+    }
+
+    public function store(CreateRequest $request)
+    {
         $validatedData = $request->validated();
 
-        $admin = User::where('email','admin@gmail.com')->first();
+        $admin = User::where('email', 'admin@gmail.com')->first();
 
         $createdProduct = Product::create([
             'title' => $validatedData['title'],
@@ -33,29 +42,41 @@ class ProductsController extends Controller
             'owner_id' => $admin->id
         ]);
         try {
-        $basePath = 'products/' . $createdProduct->id . '/';
+            $basePath = 'products/' . $createdProduct->id . '/';
 
-        $sourceImageFullPath = $basePath . 'source_url_' . $validatedData['source_url']->getClientOriginalName();
+            $sourceImageFullPath = $basePath . 'source_url_' . $validatedData['source_url']->getClientOriginalName();
 
-        $images = [
-            'thumbnail_url' => $validatedData['thumbnail_url'],
-            'demo_url' => $validatedData['demo_url']
-        ];
+            $images = [
+                'thumbnail_url' => $validatedData['thumbnail_url'],
+                'demo_url' => $validatedData['demo_url']
+            ];
 
-        ImageUploader::upload($validatedData['source_url'],$sourceImageFullPath, 'local_storage');
-        $imagePath = ImageUploader::uploadMany($images, $basePath);
+            ImageUploader::upload($validatedData['source_url'], $sourceImageFullPath, 'local_storage');
+            $imagePath = ImageUploader::uploadMany($images, $basePath);
 
-        $updatedProduct = $createdProduct->update([
-            'thumbnail_url' => $imagePath['thumbnail_url'] ,
-        'demo_url' => $imagePath['demo_url'] ,
-            'source_url' => $sourceImageFullPath
-        ]);
-        if (!$updatedProduct)
-            throw new \Exception('تصاویر آپلود نشدند');
-        return back()->with('success', 'محصول ایجاد شد');
+            $updatedProduct = $createdProduct->update([
+                'thumbnail_url' => $imagePath['thumbnail_url'],
+                'demo_url' => $imagePath['demo_url'],
+                'source_url' => $sourceImageFullPath
+            ]);
+            if (!$updatedProduct)
+                throw new \Exception('تصاویر آپلود نشدند');
+            return back()->with('success', 'محصول ایجاد شد');
 
-    } catch (\Exception $e){
-        return back()->with('failed', $e->getMessage());
+        } catch (\Exception $e) {
+            return back()->with('failed', $e->getMessage());
         }
-            }
+    }
+
+    public function downloadDemo($product_id)
+    {
+        $product = Product::findOrFail($product_id);
+        return response()->download(public_path($product->demo_url));
+    }
+
+    public function downloadSource($product_id)
+    {
+        $product = Product::findOrFail($product_id);
+        return response()->download(storage_path('app/local_storage/' . $product->source_url));
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,12 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function owner(){
+        return $this->belongsTo(User::class,'owner_id');
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
